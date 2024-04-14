f2cb95016b87d13214b3e9bbe69ea8cd3d971cdc
dali1999 <dali1999@naver.com>
2024-04-15 02:16:36 +0900

    feat: api 요청 로직 추가

---
+++ pages/api/api.ts
@@ -0,0 +1,42 @@
+const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1';
+
+export async function getRandomCocktail() {
+  try {
+    const url = `${BASE_URL}/random.php`;
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log('a');
+    return null;
+  }
+}
+
+export async function getCocktailByName(name: string) {
+  const url = `${BASE_URL}/search.php?s=${name}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
+export async function getCocktailByFirstLetter(letter: string) {
+  const url = `${BASE_URL}/search.php?f=${letter}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
+export async function getIngredientByName(name: string) {
+  const url = `${BASE_URL}/search.php?i=${name}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
+export async function getCocktailDetailsById(id: string) {
+  const url = `${BASE_URL}/lookup.php?i=${id}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
+export async function getIngredientDetailsById(id: string) {
+  const url = `${BASE_URL}/lookup.php?iid=${id}`;
+  const response = await fetch(url);
+  return response.json();
+}
